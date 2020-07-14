0ea005681fae7d96beb2cfbc490f76bd02b36234
Jy <yjyao1112@gmail.com>
2020-07-14 23:40:24 +0000

    feat: add italic tags around author names

--- styled_author.py
+++ styled_author.py
@@ -1,4 +1,6 @@
 from ehforwarderbot import Middleware
+from ehforwarderbot import coordinator
+from functools import wraps
 import logging
 
 from .__version__ import __version__ as version
@@ -14,3 +16,16 @@ class StyledAuthor(Middleware):
 
   def __init__(self, instance_id=None):
     super().__init__(instance_id=instance_id)
+    etm = coordinator.master
+    (etm.slave_messages.
+     generate_message_template) = self._override_generate_message_template(
+       etm.slave_messages.generate_message_template)
+
+  def _override_generate_message_template(self, old_generate_message_template):
+    @wraps(old_generate_message_template)
+    def wrapper(*args, **kwargs):
+      msg_template = old_generate_message_template(*args, **kwargs)
+      if msg_template:
+        msg_template = f'<i>{msg_template}</i>'
+      return msg_template
+    return wrapper
